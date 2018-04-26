4e2369128a93bff239be4604e25af70683dfd5d2
Nicola Sartorato <niksart@airmail.cc>
2018-04-26 21:11:36 +0200

    dbms postgres added DBManager for managing connections and cursors

---
+++ DBManager.py
@@ -0,0 +1,16 @@
+import psycopg2 as pg
+
+class DBManager():
+
+	def __init__(self, dbname, user, password, host):
+		self.conn = pg.connect(dbname=dbname, user=user, password=password, host=host)
+
+	def get_cursor(self):
+		return self.conn.cursor()
+
+	def close_cursor(self, cursor):
+		cursor.close()
+
+	def close_connection(self):
+		self.conn.commit()  # make changes persistent
+		self.conn.close()
--- cem.py
+++ cem.py
@@ -2,45 +2,39 @@
 # -*- coding: utf-8 -*-
 import telepot
 import telepot.routing
-import sqlite3
+import telepot.loop
+import time
+import sys
 import os
-
-dbfile = "../cem.db"
+import DBManager
 
 
 def authorize(bot, user, chat, args):
 	authorizer = user["username"]
-	authorizee = args[0]
+	authorized = args[0]
 
-	conn = sqlite3.connect(dbfile)
-	db = conn.cursor()
-	db.execute("SELECT id FROM authorizations WHERE authorizer=:azer AND authorizee=:azee",
-	           {"azer": authorizer, "azee": authorizee})
-	if db.fetchone() is not None:
+	cur = dbman.get_cursor()
+	cur.execute("SELECT id FROM authorizations WHERE authorizer=%s AND authorized=%s", (authorizer, authorized))
+	if cur.fetchone() is not None:
 		bot.sendMessage(chat["id"], "You have already authorized this user.")
 	else:
-		db.execute("INSERT INTO authorizations (authorizer, authorizee) VALUES (?, ?)", (authorizer, authorizee))
-		bot.sendMessage(chat["id"], "User @" + authorizee + " has been authorized.")
-	conn.commit()
-	db.close()
+		cur.execute("INSERT INTO authorizations (authorizer, authorized) VALUES (%s, %s)", (authorizer, authorized))
+		bot.sendMessage(chat["id"], "User @" + authorized + " has been authorized.")
+	dbman.close_cursor(cur)
 
 
 def deauthorize(bot, user, chat, args):
 	deauthorizer = user["username"]
-	deauthorizee = args[0]
+	deauthorized = args[0]
 
-	conn = sqlite3.connect(dbfile)
-	db = conn.cursor()
-	db.execute("SELECT id FROM authorizations WHERE authorizer=:authorizer AND authorizee=:authorizee",
-	           {"authorizer": deauthorizer, "authorizee": deauthorizee})
-	if db.fetchone() is None:
+	cur = dbman.get_cursor()
+	cur.execute("SELECT id FROM authorizations WHERE authorizer=%s AND authorized=%s", (deauthorizer, deauthorized))
+	if cur.fetchone() is None:
 		bot.sendMessage(chat["id"], "You have not already authorized this user. You can't deauthorize it.")
 	else:
-		db.execute("DELETE FROM authorizations WHERE authorizer=:deauthorizer AND authorizee=:deauthorizee",
-		           {"deauthorizer": deauthorizer, "deauthorizee": deauthorizee})
-		bot.sendMessage(chat["id"], "User @" + deauthorizee + " has been deauthorized.")
-	conn.commit()
-	db.close()
+		cur.execute("DELETE FROM authorizations WHERE authorizer=%s AND authorized=%s", (deauthorizer, deauthorized))
+		bot.sendMessage(chat["id"], "User @" + deauthorized + " has been deauthorized.")
+	dbman.close_cursor(cur)
 
 
 commands_private = {"authorize": authorize, "deauthorize": deauthorize}
@@ -51,6 +45,7 @@ def handle(bot, msg):
 	parsed = telepot.routing.by_chat_command(pass_args=True, separator=' ')(msg)
 
 	if parsed[0] in commands_private:
+		print(msg)
 		chat = msg["chat"]
 		user = msg["from"]
 		if chat["type"] != "private":
@@ -59,10 +54,27 @@ def handle(bot, msg):
 			commands_private[parsed[0]](bot, user, chat, parsed[1][0])
 
 
-def main():
+def main(argv):
+	global dbman
+	if len(argv) == 4+1:
+		dbname = argv[1]
+		dbuser = argv[2]
+		dbpassword = argv[3]
+		dbhost = argv[4]
+	else:
+		print("Usage: python " + argv[0] + " <dbname> <dbuser> <dbpassword> <dbhost>")
+		exit(1)
+	dbman = DBManager.DBManager(dbname, dbuser, dbpassword, dbhost)
+	
 	bot = telepot.Bot(os.environ["CEM"])
-	bot.message_loop(lambda x: handle(bot, x), run_forever=True)
+	telepot.loop.MessageLoop(bot, handle=(lambda msg: handle(bot, msg))).run_as_thread()
+	print('Listening ...')
+
+	while input() != "q":
+		time.sleep(1)
+
+	dbman.close_connection()
 
 
 if __name__ == "__main__":
-	main()
+	main(sys.argv)
