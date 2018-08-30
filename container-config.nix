544aca7f63859840cbd63e9676b2d496c9eb0bbf
Francesco Gazzetta <francygazz@gmail.com>
2018-08-30 21:14:53 +0200

    Add system configuration for cembot

---
+++ container-config.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+let
+  cembotPackage = pkgs.callPackage ./package.nix {};
+  language = "IT";
+  dbname = "cem";
+  dbuser = "cem";
+  dbpassword = import ./db-password.nix;
+  dbhost = "localhost";
+
+in {
+  services.postgresql = {
+    enable = true;
+    initialScript = pkgs.writeText "postgres-initial-script" (''
+      CREATE ROLE cem SUPERUSER LOGIN PASSWORD '${dbpassword}';
+      CREATE DATABASE cem;
+    ''
+    + builtins.readFile ./schema.sql);
+  };
+  systemd.services.cembot = {
+    description = "cembot main service";
+    path  = [ cembotPackage ];
+    environment = {
+      CEM_IT = import ./telegram-token.nix;
+    };
+    script =
+      ''
+        cem \
+          ${dbname} \
+          ${dbuser} \
+          ${dbpassword} \
+          ${dbhost} \
+          ${language}
+      '';
+  };
+}
+
