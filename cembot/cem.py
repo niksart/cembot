c35ba7857c67a6f8daea886040afad08285b432b
Francesco Gazzetta <francygazz@gmail.com>
2018-09-03 15:01:15 +0200

    Use blocking MessageLoop and cleanup on exception

--- cembot/cem.py
+++ cembot/cem.py
@@ -510,13 +510,10 @@ def main(argv):
 		bot = telepot.Bot(os.environ[bot_env_variabile])
 		bot_id = bot.getMe()["id"]
 
-		telepot.loop.MessageLoop(bot, handle=(lambda msg: handle(bot, msg))).run_as_thread()
-		print('Listening ...')
-
-		while input() != "q":
-			time.sleep(1)
-
-		dbman.close_connection()
+		try:
+			telepot.loop.MessageLoop(bot, handle=(lambda msg: handle(bot, msg))).run_forever()
+		except:
+			dbman.close_connection()
 	else:
 		print("Usage: python " + argv[0] + " <dbname> <dbuser> <dbpassword> <dbhost> <IT | EN for language>")
 		exit(1)
